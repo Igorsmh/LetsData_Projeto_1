707b8800c4aec4b07e5f646d0260a9e3e7a9346b
Igorsmh <86760061+Igorsmh@users.noreply.github.com>
2023-03-20 01:35:16 -0300

    Delete notebooks directory

--- notebooks/converter_parquet.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import pyarrow
-
-
-df_convert = pd.read_csv(r'C:\Users\igors\OneDrive\Documents\Estudo_python\Jornada_cientista_de_dados\Projeto_1\dados\MICRODADOS_ENEM_2019.csv',sep=';', encoding='latin1')
-df_convert.to_parquet(r'C:\Users\igors\OneDrive\Documents\Estudo_python\Jornada_cientista_de_dados\Projeto_1\dados\MICRODADOS_ENEM_2019.parquet')
