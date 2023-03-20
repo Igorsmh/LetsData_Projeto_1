8f2d88dbe22215bb34a6bebacf1025d29c469065
Igor <shenriques@ihm.com.br>
2023-03-20 16:29:14 -0300

    t

--- Scr/converter_parquet.py
+++ Scr/converter_parquet.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import pyarrow
 
 
 df_convert = pd.read_csv(r'C:\Users\igors\OneDrive\Documents\Estudo_python\Jornada_cientista_de_dados\Projeto_1\dados\MICRODADOS_ENEM_2019.csv',sep=';', encoding='latin1')
